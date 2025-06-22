812847f4190e6eed99bcb4cf90dc89928d8ced96
Hubert Khouzam <hub.khouzam@gmail.com>
2025-06-22 09:46:15 -0400

    added upload to s3

--- server/ML/main.py
+++ server/ML/main.py
@@ -14,6 +14,7 @@ import socket
 from collections import deque, Counter
 import os
 import glob
+import boto3
 
 from deepface import DeepFace
 import cv2
@@ -21,7 +22,15 @@ from services.intern import process_video
 from services.transcript import transcribe_video
 from services.clip_processor import ClipProcessor
 
+from dotenv import load_dotenv
+load_dotenv()
 
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    region_name=os.getenv('AWS_REGION')
+)
 
 ###############################################################################
 # ---------------------------  Emotion Worker  --------------------------------
@@ -230,6 +239,14 @@ def process_single_clip(clip_path: str, processor: ClipProcessor, clip_id: str):
     is_viral = processor.check_viral_status(clip_id)
     return is_viral
 
+
+def upload_file_to_s3(file_path, bucket_name, s3_key):
+    try:
+        s3.upload_file(file_path, bucket_name, s3_key)
+        print(f"File uploaded to s3://{bucket_name}/{s3_key}")
+    except Exception as e:
+        print(f"Upload failed: {e}")
+
 def run(clips_dir: str):
     processor = ClipProcessor()
     processed_files = set()
@@ -279,6 +296,8 @@ def run(clips_dir: str):
             
     except KeyboardInterrupt:
         print("\n[Main] Shutting down...")
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Viral Clip Detector")
