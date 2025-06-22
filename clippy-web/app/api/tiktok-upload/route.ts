211bd55d30648870f33b2fb610a0a65e3d063ae8
Kevin He <ipman4567@gmail.com>
2025-06-21 22:18:19 -0400

    ok

--- clippy-web/app/api/tiktok-upload/route.ts
+++ clippy-web/app/api/tiktok-upload/route.ts
@@ -5,14 +5,26 @@ export async function POST(req: NextRequest) {
     const { access_token, source_info } = await req.json()
 
     const response = await fetch(
-      'https://open.tiktokapis.com/v2/post/publish/inbox/video/init/',
+      'https://open.tiktokapis.com/v2/post/publish/video/init/',
       {
         method: 'POST',
         headers: {
           Authorization: `Bearer ${access_token}`, // Use token from request
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ source_info }),
+        body: JSON.stringify({
+          post_info: {
+            title: 'this will be a funny #cat video on your @tiktok #fyp',
+            privacy_level: 'SELF_ONLY',
+            disable_duet: false,
+            disable_comment: true,
+            disable_stitch: false,
+            video_cover_timestamp_ms: 1000,
+          },
+          source_info: {
+            source: 'public/vid.mov', // Use the source from request
+          },
+        }),
       }
     )
 
